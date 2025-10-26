5f6803ef29b7fcd96f096e39c70de8804993530c
Briannjugi <machariabrian644@gmail.com>
2025-10-26 14:40:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// ===== MOBILE MENU TOGGLE =====
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
